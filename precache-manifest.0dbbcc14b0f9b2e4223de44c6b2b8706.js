1c4d062b69ab51ef0c3207fc517a1c50414cdafe
kvoncode <kvoncode@gmail.com>
2019-08-20 15:34:37 +0900

    Updates

---
+++ precache-manifest.0dbbcc14b0f9b2e4223de44c6b2b8706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536b7550f35fccd0ee7b22b1ee964b18",
+    "url": "/cra-ghpages/index.html"
+  },
+  {
+    "revision": "48a40c2ea0412ee8d155",
+    "url": "/cra-ghpages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c986ad781151b73245a2",
+    "url": "/cra-ghpages/static/js/2.d8d33950.chunk.js"
+  },
+  {
+    "revision": "48a40c2ea0412ee8d155",
+    "url": "/cra-ghpages/static/js/main.dd39678f.chunk.js"
+  },
+  {
+    "revision": "29b0d373408e1af93172",
+    "url": "/cra-ghpages/static/js/runtime~main.3a40b53a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra-ghpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
